50ce2845bd87969ad9d640a3bfe631bfc887f375
Joao Victor <jvso@yahoo.com>
2024-04-29 03:58:15 -0300

    Logs de info em Aluno e Cadastro Controllers

--- src/main/java/com/fullstack/educacional/controller/AlunoController.java
+++ src/main/java/com/fullstack/educacional/controller/AlunoController.java
@@ -6,6 +6,7 @@ import com.fullstack.educacional.datasource.entity.AlunoEntity;
 import com.fullstack.educacional.datasource.entity.NotaEntity;
 import com.fullstack.educacional.service.AlunoServiceImpl;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,41 +15,49 @@ import java.util.List;
 @RestController
 @RequestMapping("alunos")
 @RequiredArgsConstructor
+@Slf4j
 public class AlunoController {
     private final AlunoServiceImpl service;
 
     @GetMapping("{id}")
     public ResponseEntity<AlunoEntity> get(@PathVariable Long id, @RequestHeader(name = "Authorization") String token) {
+        log.info("GET /alunos/{} -> Buscando aluno", id);
         return ResponseEntity.ok(service.get(id, token));
     }
 
     @GetMapping
     public ResponseEntity<List<AlunoEntity>> get() {
+        log.info("GET /alunos -> Buscando todos os alunos");
         return ResponseEntity.ok(service.getAll());
     }
 
     @PostMapping
     public ResponseEntity<AlunoEntity> post(@RequestBody AlunoRequest alunoRequest) {
+        log.info("POST /alunos -> Criando um novo aluno");
         return ResponseEntity.ok(service.create(alunoRequest));
     }
 
     @PutMapping("{id}")
     public ResponseEntity<AlunoEntity> put(@PathVariable Long id, @RequestBody AlunoRequest alunoRequest) {
+        log.info("PUT /alunos/{} -> Alterando dados do aluno", id);
         return ResponseEntity.ok(service.alter(id, alunoRequest));
     }
 
     @DeleteMapping("{id}")
     public ResponseEntity<String> delete(@PathVariable Long id) {
+        log.info("DELETE /alunos/{} -> Deletando aluno", id);
         return ResponseEntity.ok(service.delete(id));
     }
 
     @GetMapping("{id}/notas")
     public ResponseEntity<List<NotaEntity>> getAllNotas(@PathVariable Long id, @RequestHeader(name = "Authorization") String token) {
+        log.info("GET /alunos/{}/notas -> Buscando todas as notas do aluno", id);
         return ResponseEntity.ok(service.getAllNotas(id, token));
     }
 
     @GetMapping("{id}/pontuacao")
     public ResponseEntity<PontuacaoResponse> getPontuacaoTotal(@PathVariable Long id, @RequestHeader(name = "Authorization") String token) {
+        log.info("GET /alunos/{}/pontuacao -> Buscando pontuação total do aluno", id);
         return ResponseEntity.ok(service.getPontuacaoTotal(id, token));
     }
 }
--- src/main/java/com/fullstack/educacional/controller/CadastroController.java
+++ src/main/java/com/fullstack/educacional/controller/CadastroController.java
@@ -6,6 +6,7 @@ import com.fullstack.educacional.datasource.entity.UsuarioEntity;
 import com.fullstack.educacional.datasource.repository.PapelRepository;
 import com.fullstack.educacional.service.UsuarioServiceImpl;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class CadastroController {
     private final UsuarioServiceImpl usuarioService;
     private final PapelRepository papelRepository;
@@ -22,32 +24,38 @@ public class CadastroController {
     public ResponseEntity<String> novoUsuario(
             @Validated @RequestBody UsuarioRequest usuarioRequest
     ) {
+        log.info("POST /cadastro -> Criando novo usuário");
         return ResponseEntity.ok("Usuário Salvo! ID do usuário: "+ usuarioService.create(usuarioRequest).getId());
     }
 
     @PostMapping("papel")
     public ResponseEntity<String> papel(@RequestBody PapelEntity papel) {
+        log.info("POST /papel -> Criando novo papel");
         papelRepository.save(papel);
         return ResponseEntity.ok("Papel salvo com sucesso!");
     }
 
     @GetMapping("usuarios")
     public ResponseEntity<List<UsuarioEntity>> getUsuarios() {
+        log.info("GET /usuarios -> Buscando todos os usuários");
         return ResponseEntity.ok(usuarioService.getAll());
     }
 
     @GetMapping("usuarios/{id}")
     public ResponseEntity<UsuarioEntity> getUsuario(@PathVariable Long id) {
+        log.info("GET /usuarios/{} -> Buscando usuário", id);
         return ResponseEntity.ok(usuarioService.get(id));
     }
 
     @PutMapping("usuarios/senha")
     public ResponseEntity<String> alterUsuarioSenha(@RequestHeader(name = "Authorization") String token, @RequestBody String senha) {
+        log.info("PUT /usuarios/senha -> Alterando senha do usuário logado");
         return ResponseEntity.ok(usuarioService.alterSenha(token, senha));
     }
 
     @DeleteMapping("usuarios/{id}")
     public ResponseEntity<String> deleteUsuario(@PathVariable Long id) {
+        log.info("DELETE usuarios/{} -> Deletando usuário", id);
         return ResponseEntity.ok(usuarioService.delete(id));
     }
 }
