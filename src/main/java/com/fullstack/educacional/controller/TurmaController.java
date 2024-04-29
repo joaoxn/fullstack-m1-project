747061445e65d14656cb96ff81c28db54e44d622
Joao Victor <jvso@yahoo.com>
2024-04-29 19:00:17 -0300

    Logs feitos para Controllers faltantes

--- src/main/java/com/fullstack/educacional/controller/TurmaController.java
+++ src/main/java/com/fullstack/educacional/controller/TurmaController.java
@@ -4,6 +4,7 @@ import com.fullstack.educacional.controller.dto.request.TurmaRequest;
 import com.fullstack.educacional.datasource.entity.TurmaEntity;
 import com.fullstack.educacional.service.TurmaServiceImpl;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,21 +13,28 @@ import java.util.List;
 @RestController
 @RequestMapping("turmas")
 @RequiredArgsConstructor
+@Slf4j
 public class TurmaController {
     private final TurmaServiceImpl service;
 
     @GetMapping("{id}")
     public ResponseEntity<TurmaEntity> get(@PathVariable Long id) {
+        log.info("GET /turmas/{} -> Buscando turma por id", id);
         return ResponseEntity.ok(service.get(id));
     }
 
     @GetMapping
     public ResponseEntity<List<TurmaEntity>> get() {
-        return ResponseEntity.ok(service.getAll());
+        log.info("GET /turmas -> Buscando todas as turmas");
+        List<TurmaEntity> turmas = service.getAll();
+        log.debug("GET /turmas -> Encontrado {} turmas", turmas.size());
+        return ResponseEntity.ok(turmas);
     }
 
     @PostMapping
     public ResponseEntity<TurmaEntity> post(@RequestBody TurmaRequest turmaData) {
+        log.info("POST /turmas -> Criando turma com nome: {}", turmaData.nome());
+        log.info("POST /turmas -> Criando turma:\nID do Curso: {},\nID do Docente: {}", turmaData.cursoId(), turmaData.docenteId());
         return ResponseEntity.ok(service.create(turmaData));
     }
 
@@ -35,11 +43,15 @@ public class TurmaController {
             @PathVariable Long id,
             @RequestBody TurmaRequest turmaData
     ) {
+        log.info("PUT /turmas/{} -> Alterando turma por id", id);
+        log.debug("PUT /turmas/{} -> Alterando turma por id:\nNome: {},\nID do Curso: {},\nID do Docente: {}",
+                id, turmaData.nome(), turmaData.cursoId(), turmaData.docenteId());
         return ResponseEntity.ok(service.alter(id, turmaData));
     }
 
     @DeleteMapping("{id}")
     public ResponseEntity<String> delete(@PathVariable Long id) {
+        log.info("DELETE /turmas/{} -> Deletando turma por id", id);
         return ResponseEntity.ok(service.delete(id));
     }
 }
